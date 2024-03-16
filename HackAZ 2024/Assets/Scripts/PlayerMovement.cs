5fd7d28787cde18574ee20a27154e3903466cd63
NathanK29 <nathanakumar04@gmail.com>
2024-03-16 16:26:06 -0700

    Movement/Animations on Penguin

--- HackAZ 2024/Assets/Scripts/PlayerMovement.cs
+++ HackAZ 2024/Assets/Scripts/PlayerMovement.cs
@@ -1,54 +1,173 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    private float horizontal;
-    private float speed = 4f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
-
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float jumpingPower = 16f;
+    [SerializeField] private float jumpBufferTime = 0.1f;
+    [SerializeField] private float coyoteTime = 0.2f;
     [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
+    private Rigidbody2D rb;
+    private CapsuleCollider2D col;
+    private Animator animator;
+    private float lastGroundedTime;
+    private float lastJumpTime;
+    private bool isGrounded;
 
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        isGrounded = IsGrounded();
+
+        if (isGrounded)
         {
+            lastGroundedTime = Time.time;
+            animator.SetBool("IsJumping", false); 
+        }
+        else
+        {
+            animator.SetBool("IsJumping", true); 
+        }
+
+        if (Input.GetButtonDown("Jump") && (Time.time - lastGroundedTime <= coyoteTime))
+        {
+            lastJumpTime = Time.time;
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
-        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f);
-        }
 
-        Flip();
+        Flip(horizontal);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Vector2 normal = collision.GetContact(0).normal;
+            
+            if (normal.y > 0.5)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Vector2 normal = collision.GetContact(0).normal;
+
+            // Check if the normal is mostly vertical (ground)
+            if (Mathf.Abs(normal.y) > 0.5)
+            {
+                isGrounded = true;
+            }
+            else // if the normal is mostly horizontal (wall)
+            {
+                // Make the player start falling if they're on a wall
+                if (isGrounded && Mathf.Abs(normal.y) <= 0.5)
+                {
+                    // Force the player to fall
+                    rb.velocity = new Vector2(rb.velocity.x, -1f);
+                    isGrounded = false; // They are not grounded, they're on a wall
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        if (col == null) return false;
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(col.bounds.center, col.size, CapsuleDirection2D.Vertical, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 
-    private void Flip()
+    private void Flip(float horizontal)
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+        if (horizontal > 0f && transform.localScale.x < 0f || horizontal < 0f && transform.localScale.x > 0f)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
     }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerMovement : MonoBehaviour
+//{
+
+//    private float horizontal;
+//    private float speed = 4f;
+//    private float jumpingPower = 16f;
+//    private bool isFacingRight = true;
+
+
+//    [SerializeField] private Rigidbody2D rb;
+//    [SerializeField] private Transform groundCheck;
+//    [SerializeField] private LayerMask groundLayer;
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        horizontal = Input.GetAxisRaw("Horizontal");
+
+//        if(Input.GetButtonDown("Jump") && IsGrounded())
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+//        }
+//        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f);
+//        }
+
+//        Flip();
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+//    }
+
+//    private bool IsGrounded()
+//    {
+//        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+//    }
+
+//    private void Flip()
+//    {
+//        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
+//            isFacingRight = !isFacingRight;
+//            Vector3 localScale = transform.localScale;
+//            localScale.x *= -1f;
+//            transform.localScale = localScale;
+//        }
+//    }
+//}
---
+++ HackAZ 2024/Assets/Tilemap/SnowTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SnowTiles" tilewidth="16" tileheight="16" tilecount="170" columns="17">
+ <image source="../../../../../../Desktop/basic-platformer-tileset_v1b.png" width="272" height="160"/>
+</tileset>
