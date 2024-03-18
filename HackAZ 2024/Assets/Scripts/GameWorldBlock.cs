fdc148c7db3effdc332c0d2f1b85a594ecfe4cf3
andyelu <andylu@arizona.edu>
2024-03-17 22:35:26 -0700

    got letters working finally

--- HackAZ 2024/Assets/Scripts/GameWorldBlock.cs
+++ HackAZ 2024/Assets/Scripts/GameWorldBlock.cs
@@ -9,12 +9,33 @@ public class GameWorldBlock : MonoBehaviour
     [SerializeField]
     private bool isClicked = false;
     private BoxCollider2D[] boxes;
-    private TextMeshProUGUI text;
+    private TextMeshProUGUI[] texts; // To store TextMeshProUGUI components of children
+
+    // Removed the letter property since we now deal with multiple letters
 
     private void Start()
     {
         boxes = GetComponentsInChildren<BoxCollider2D>();
-        text = GetComponentInChildren<TextMeshProUGUI>();
+        texts = GetComponentsInChildren<TextMeshProUGUI>(); // Get all TextMeshProUGUI components in children
+
+        // Ensure we have enough characters in our string for all children
+        if (References.currentString.Length >= texts.Length)
+        {
+            for (int i = 0; i < texts.Length; i++)
+            {
+                if (References.counter < References.currentString.Length)
+                {
+                    // Assign a letter to each TextMeshProUGUI component from the string
+                    char letter = References.currentString[References.counter];
+                    texts[i].text = letter.ToString();
+                    References.counter++; // Move to the next character for the next child
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Not enough letters in References.currentString for all children.");
+        }
     }
 
     void Update()
@@ -27,14 +48,14 @@ public class GameWorldBlock : MonoBehaviour
                 collider.enabled = false;
             }
 
-            Debug.Log("Moving");
+            // Debug.Log("Moving");
             float distanceFromCamera = Mathf.Abs(Camera.main.transform.position.z - transform.position.z);
-
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceFromCamera));
             transform.position = worldPosition;
 
             if (Input.GetMouseButtonDown(0))
             {
+                ResetCounterAndString();
                 isClicked = true;
                 foreach (var collider in boxes)
                 {
@@ -43,4 +64,12 @@ public class GameWorldBlock : MonoBehaviour
             }
         }
     }
+
+    private void ResetCounterAndString()
+    {
+        References.currentString = string.Empty;
+        References.counter = 0;
+    }
+
+    // Removed the SetLetter method since it's no longer needed for this use case
 }
--- HackAZ 2024/Assets/Scripts/Tracker.cs
+++ HackAZ 2024/Assets/Scripts/Tracker.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 
 public static class Tracker
 {
-    private static HashSet<string> _words = new HashSet<string>();
+    //private static HashSet<string> _words = new HashSet<string>();
 
-    public static void Add(string word)
-    {
-        return _words.Add(word);
-    }
+    //public static void Add(string word)
+    //{
+    //    return _words.Add(word);
+    //}
 
-    public static void Reset()
-    {
-        _words.Clear();
-    }
+    //public static void Reset()
+    //{
+    //    _words.Clear();
+    //}
 }
