2bd5210c532ae5e71e04eac6c4d57b8b49423778
andyelu <andylu@arizona.edu>
2024-03-19 23:55:52 -0700

    experiment with composite colliders in sample scene

--- HackAZ 2024/Assets/Scripts/LetterBankScript.cs
+++ HackAZ 2024/Assets/Scripts/LetterBankScript.cs
@@ -1,7 +1,5 @@
-using JetBrains.Annotations;
+
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.SceneManagement;
